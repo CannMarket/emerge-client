f283076e8b6610b98bb6084da88a8f47ff47a4d4
Mike Henken <mikehenken@gmail.com>
2016-04-17 09:21:02 -0400

    Final COmmit

--- www/app/account/AccountCtrl.js
+++ www/app/account/AccountCtrl.js
@@ -1,11 +1,22 @@
-controllers.controller('AccountCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-
+controllers.controller('AccountCtrl', function($scope, $http) {
+  $scope.user = {
+  	name 		: 'Mike Henken',
+  	email 		: 'mikehenken@gmail.com',
+  	phone		  : '239-810-0119',
+  	address1	: '8320 Matanzas Rd',
+  	city		   : 'Fort Myers',
+  	state		  : 'Florida',
+  	zip			 : '33967',
+  	country		: 'USA'
+  };
+  $scope.name = 'test';
+  $scope.success = false;
+  $scope.submit = function(){
+    $scope.success = true;
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.success = false;
+    }, 1000);
+  };
 })
--- www/app/payments/PaymentsCtrl.js
+++ www/app/payments/PaymentsCtrl.js
@@ -1,3 +1,17 @@
 controllers.controller('PaymentsCtrl', function($scope, $stateParams) {
   console.log($stateParams);
+  $scope.payment_success = false;
+  $scope.contacts = [
+    { name: 'Evan Wagner', id: 1 },
+    { name: 'David Cruz', id: 2 },
+    { name: 'Ann Taylor', id: 3 }
+  ];
+  $scope.submitPayment = function(){
+  	$scope.payment_success = true;
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+  		$scope.payment_success = false;
+    }, 1000);
+  };
 })
